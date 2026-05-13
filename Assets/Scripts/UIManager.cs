c1e779680c5656842d71758d7e5eb50449b4bcc5
Tiago Neto <tiagofneto@icloud.com>
2026-05-13 10:56:53 +0100

    Start implementing user interface

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    // Method to switch scenes
+    public void LoadARScene()
+    {
+        Debug.Log("O botão foi clicado com sucesso!");
+        SceneManager.LoadScene("AR");
+    }
+}
